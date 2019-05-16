88211b713fb6325b2c6d80e1ab8d4db200f29b88
alux <ahmadabd13741112@gmail.com>
2019-05-16 22:48:08 +0430

    guess random number

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+
+    // let mut tmp = 2;
+    // let bar = tmp;
+
+    // println!("bar: {}", bar);
+    // tmp = tmp + 1;
+    // println!("bar: {}", bar);
+    // println!("tmp: {}", tmp);
+
+    //----------------------------
+    
+    // Get a random number between 1 to 100
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop{    // Make a loop 
+
+        println!("Enter a number:");
+        let mut guess = String::new();
+
+        // Get a value from user
+        io::stdin().read_line(&mut guess)
+            .expect("Fail to read");
+
+        // let guess: u32 = guess.trim().parse()
+        //    .expect("cannot convert");
+
+        // Convert string to integer
+        let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+        };
+
+        // Make a condition for comparing random number and user input
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Equal :) YOU WIN");
+                break;
+            }
+        }
+    }
+}
